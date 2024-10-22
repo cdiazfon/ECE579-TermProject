b9cb92cdf263ad93765fd92bdacd6961e3217c04
Stayner R <66812128+Staynerr03@users.noreply.github.com>
2024-10-21 21:42:40 -0400

    Test commit

--- TrafficSignDetectionNN/train.py
+++ TrafficSignDetectionNN/train.py
@@ -29,7 +29,7 @@ def main():
     #     print(image)
 
     #plot_images(X_train, y_train)
-
+    #Test comment
     # TODO: need to convert image vectors into pytorch arrays
     # TODO: create neural network dimensions
     # TODO: set learning hyperparameters e.g(learning rate, epochs, batch size)
